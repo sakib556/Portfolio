0f95c68666bf1fd2d4654b92ec58911936fbba12
sakib556 <kazisakib556@gmail.com>
2024-08-31 13:10:58 +0600

    portfolio screen design

---
+++ lib/portfolio_screen.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:portfolio/details_section.dart';
+import 'package:portfolio/white_box.dart';
+
+class PortfolioScreen extends StatelessWidget {
+  const PortfolioScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Image.asset("assets/men.jpg"),
+            SizedBox(
+              height: 10,
+            ),
+            const Text(
+              "Mr Sohel Rana",
+              style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.white),
+            ),
+            WhiteBox(title: "Email :", value: "mrsohelrana@gmail.com"),
+            SizedBox(
+              height: 10,
+            ),
+            WhiteBox(title: "Github :", value: "www.github.com/sakib556"),
+            SizedBox(
+              height: 10,
+            ),
+            const DetailsSection(
+                title1: "Intitute Name :",
+                value1: "Feni Computer Institute",
+                title2: "Department :",
+                value2: "Computer Science",
+                title3: "ID No :",
+                value3: "11202154",
+                title4: "Birthday :",
+                value4: "03-07-2000")
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// column >
+
+// image
+// title
+// description
+//-------------------------------------------------
+
+
+
+
+
+
+//--------------------------------------------------------
---
+++ lib/white_box.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class WhiteBox extends StatelessWidget {
+  const WhiteBox({super.key, required this.title, required this.value});
+  final String title;
+  final String value;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(left: 20, right: 20),
+      child: Container(
+        decoration: BoxDecoration(
+            color: Colors.white, borderRadius: BorderRadius.circular(10)),
+        padding: EdgeInsets.all(10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(title),
+            SizedBox(
+              width: 4,
+            ),
+            Text(value)
+          ],
+        ),
+      ),
+    );
+  }
+}
+// ---------------------------
+
+// row :  Username :________Mr Rana
+
+//-----------------------------
+
+// ---------------------------
+
+// Institute name : Acasdfkd
+
+//-----------------------------
